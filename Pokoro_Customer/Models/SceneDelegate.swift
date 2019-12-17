0cb4ac870cc518ccbaaf6b6ba639d5bf9dabc425
Reza Bina <tavakolibina@yahoo.com>
2019-12-17 15:44:36 +0200

    + App Icons

--- Pokoro_Customer/Models/SceneDelegate.swift
+++ Pokoro_Customer/Models/SceneDelegate.swift
@@ -16,9 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        makeAuthenticationCoordinator(windowScene)
+        presentAuthenticationCoordinator()
+    }
+    
+    private func makeAuthenticationCoordinator(_ windowScene: UIWindowScene) {
         window = UIWindow(windowScene: windowScene)
         router = SceneDelegateRouter(window: window!)
         coordinator = AuthenticationCoordinator(router: router!)
+    }
+    
+    public func presentAuthenticationCoordinator() {
         coordinator?.present(animated: true, onDismissed: nil)
     }
 
