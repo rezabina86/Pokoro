bf729098e03aaa306a74b5696db7b465af04eeeb
Reza Bina <tavakolibina@yahoo.com>
2019-12-21 12:37:30 +0200

    + safeArea extension

--- Pokoro_Customer/Controllers/Login/LoginViewController.swift
+++ Pokoro_Customer/Controllers/Login/LoginViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let noticeLabel: UITextView = {
+        let label = UITextView()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = ThemeManager.shared.theme?.backgroundColor
+        setupViews()
+        test()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = ThemeManager.shared.theme?.primaryColor
+        
+        view.addSubview(noticeLabel)
+        noticeLabel.bottomAnchor.constraint(equalTo: view.safeBottomAnchor, constant: -16).isActive = true
+        noticeLabel.leadingAnchor.constraint(equalTo: view.safeLeadingAnchor, constant: 42).isActive = true
+        noticeLabel.trailingAnchor.constraint(equalTo: view.safeTrailingAnchor, constant: -42).isActive = true
+        noticeLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    private func test() {
+        let plainAttributedString = NSMutableAttributedString(string: "noticeWelcome".localized, attributes: [NSAttributedString.Key.foregroundColor : (ThemeManager.shared.theme?.textColor)!, NSAttributedString.Key.font : UIFont.PKFonts.MediumSB])
+    
+        let attributedLinkString = NSMutableAttributedString(string: "terms".localized, attributes:[NSAttributedString.Key.link: URL(string: "http://www.google.com")!, NSAttributedString.Key.foregroundColor : UIColor.blue, NSAttributedString.Key.font : UIFont.PKFonts.MediumSB])
+        
+        let plainAttributedString1 = NSMutableAttributedString(string: "and".localized, attributes: [NSAttributedString.Key.foregroundColor : (ThemeManager.shared.theme?.textColor)!, NSAttributedString.Key.font : UIFont.PKFonts.MediumSB])
+        
+        let attributedLinkString1 = NSMutableAttributedString(string: "policy".localized, attributes:[NSAttributedString.Key.link: URL(string: "http://www.google.com")!, NSAttributedString.Key.foregroundColor : UIColor.blue, NSAttributedString.Key.font : UIFont.PKFonts.MediumSB])
+        
+        let style = NSMutableParagraphStyle()
+        style.alignment = NSTextAlignment.center
+        
+        let fullAttributedString = NSMutableAttributedString()
+        fullAttributedString.append(plainAttributedString)
+        fullAttributedString.append(attributedLinkString)
+        fullAttributedString.append(plainAttributedString1)
+        fullAttributedString.append(attributedLinkString1)
+        fullAttributedString.addAttributes([NSAttributedString.Key.paragraphStyle: style], range: NSMakeRange(0, fullAttributedString.length))
+        
+        noticeLabel.isUserInteractionEnabled = true
+        noticeLabel.isEditable = false
+        noticeLabel.attributedText = fullAttributedString
     }
 
 }
---
+++ Pokoro_Customer/Models/Extensions/UIView+SafeArea.swift
@@ -0,0 +1,77 @@
+//
+//  UIView+SafeArea.swift
+//  Ronaker
+//
+//  Created by Reza Bina on 9/15/18.
+//  Copyright © 2018 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    var safeCenterXAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.centerXAnchor
+        } else {
+            return self.centerXAnchor
+        }
+    }
+    
+    var safeCenterYAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.centerYAnchor
+        } else {
+            return self.centerYAnchor
+        }
+    }
+    
+    var safeTopAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.topAnchor
+        } else {
+            return self.topAnchor
+        }
+    }
+    
+    var safeTrailingAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *){
+            return self.safeAreaLayoutGuide.trailingAnchor
+        }else {
+            return self.trailingAnchor
+        }
+    }
+    
+    var safeLeadingAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *){
+            return self.safeAreaLayoutGuide.leadingAnchor
+        }else {
+            return self.leadingAnchor
+        }
+    }
+    
+    var safeLeftAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *){
+            return self.safeAreaLayoutGuide.leftAnchor
+        }else {
+            return self.leftAnchor
+        }
+    }
+    
+    var safeRightAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *){
+            return self.safeAreaLayoutGuide.rightAnchor
+        }else {
+            return self.rightAnchor
+        }
+    }
+    
+    var safeBottomAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.bottomAnchor
+        } else {
+            return self.bottomAnchor
+        }
+    }
+    
+}
